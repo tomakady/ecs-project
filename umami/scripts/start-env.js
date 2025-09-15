a1cffb35985b50f62e026b1ae8eb31eca50b1148
tomakady <tomakady@gmail.com>
2025-09-15 13:18:41 +0100

    add app

--- umami/scripts/start-env.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const cli = require('next/dist/cli/next-start');
-
-cli.nextStart({
-  port: process.env.PORT || 3000,
-  hostname: process.env.HOSTNAME || '0.0.0.0',
-});
--- umami/src/declaration.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'bcryptjs';
-declare module 'chartjs-adapter-date-fns';
-declare module 'cors';
-declare module 'debug';
-declare module 'jsonwebtoken';
-declare module 'md5';
