4d6d936e2c957ab2ee91551f565b7d126b31b95e
tomakady <tomakady@gmail.com>
2025-10-23 13:06:57 +0100

    push

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,33 +1,3 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
-  backend "s3" {
-    bucket         = "memos-terraform-state"
-    key            = "memos/terraform.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "memos-terraform-locks"
-    encrypt        = true
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-
-  default_tags {
-    tags = {
-      Project     = var.project_name
-      Environment = var.environment
-      ManagedBy   = "terraform"
-    }
-  }
-}
 
 # # S3 Bucket for Terraform State
 # resource "aws_s3_bucket" "terraform_state" {
